4c0ac28c229d05b31c0970f985bf4aded5743341
FrostCat <fr0stcat@gmail.com>
2015-08-04 19:26:26 -0500

    Create int.cs

---
+++ int.cs
@@ -0,0 +1,13 @@
+using External;
+
+namespace Intermediate
+{
+    public class Int
+    {
+        public string GetExt()
+        {
+            ext e = new ext();
+            return e.thing();
+        }
+    }
+}
