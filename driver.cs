dd43d98880bba5a3691ada26dce3efcaee4da5d3
FrostCat <fr0stcat@gmail.com>
2015-08-04 19:26:57 -0500

    Create driver.cs

---
+++ driver.cs
@@ -0,0 +1,11 @@
+using Intermediate;
+using System;
+
+public class driver
+{
+    public static void Main(string[] args)
+    {
+        Int i = new Int();
+        Console.WriteLine("Asked for version, got [" + i.GetExt() + "]");
+    }
+}
